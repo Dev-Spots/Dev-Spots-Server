bbe9eb558f7352aadedc1e75fec710506a1a676b
anandafiqri <anandafiqri1403@gmail.com>
2024-03-14 08:38:23 +0700

    setup app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,13 @@
-import { Module } from "@nestjs/common";
-import { MongooseModule } from "@nestjs/mongoose";
-import { config } from "dotenv";
+import {
+  type MiddlewareConsumer,
+  Module,
+  type NestModule,
+} from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { config } from 'dotenv';
+import { MorganModule, MorganInterceptor } from 'nest-morgan';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { UrlEncodedParser } from '../middlewares/urlEncodedParser.middleware';
 
 config();
 
@@ -9,8 +16,20 @@ config();
     MongooseModule.forRoot(process.env.DATABASE_URL, {
       waitQueueTimeoutMS: 1000 * 30,
     }),
+    MorganModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: MorganInterceptor('combined', {
+        skip: () => process.env.NODE_ENV === 'test',
+      }),
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(UrlEncodedParser).forRoutes('*');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { GLOBAL_PREFIX } from "../constant";
+import { ForbiddenException } from "@nestjs/common";
+import helmet from "helmet";
+import { AllExceptionsFilter } from "../middlewares/errorHandler.middleware";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.use(
+    helmet({
+      referrerPolicy: { policy: "same-origin" },
+    })
+  );
+
+  app.useGlobalFilters(new AllExceptionsFilter());
+
+  app.enableCors({
+    origin(requestOrigin, callback) {
+      const whiteList = process.env.CORS_LIST ?? "";
+      if (whiteList.indexOf(requestOrigin as string) !== -1) {
+        callback(null, true);
+      } else {
+        callback(
+          new ForbiddenException(`Not allowed by CORS for URL ${requestOrigin}`)
+        );
+      }
+    },
+  });
+
+  app.setGlobalPrefix(GLOBAL_PREFIX);
+  await app.listen(3001);
 }
 bootstrap();
